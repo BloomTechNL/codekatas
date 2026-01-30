d842b5c6b3bf0629b7a1723075ab1dab85504852
Timber Kerkvliet <timberkerkvliet@gmail.com>
2026-01-30 06:12:25 +0100

    Tests passing changes

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -6,6 +6,10 @@ describe('Math functions', () => {
     expect(add(2, 3)).toBe(5);
   });
 
+  it('should add two numbers', () => {
+    expect(add(2, 10)).toBe(12);
+  });
+
   it('should multiply two numbers', () => {
     expect(multiply(2, 3)).toBe(6);
   });
